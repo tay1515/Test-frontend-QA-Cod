585a83b9cc0400cd104484ce7cc5637dae6fd3bd
estud77@gmail.com <estud77@gmail.com>
2025-03-11 11:40:53 -0500

    creacion readme

--- pages/ProductListPage.py
+++ pages/ProductListPage.py
@@ -58,7 +58,7 @@ class ProductListPage:
         list_select_product = []
         product_list = self._list_products2
 
-        if quantity_products <= product_list.count():
+        if quantity_products <= product_list.count() & quantity_products != 0:
             while True:
                 num_ramdon = random.randint(0, product_list.count() - 1)
                 item = product_list.all_inner_texts()[num_ramdon]
